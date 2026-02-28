2f5ba1437ea30e32089a29785fa819943b9b8df0
Gagan-03C <gagans0103@gmail.com>
2026-02-28 22:24:29 +0530

    b5G3

---
+++ data.js
@@ -0,0 +1,30 @@
+const sections = {
+    news: `
+        <h2>Campus Newspaper</h2>
+        <p>The Code-Red hackathon turned tragic...</p>
+    `,
+
+    notes: `
+        <h2>Detective Notes</h2>
+        <p>Initial entry shows no signs of struggle...</p>
+    `,
+
+    autopsy: `
+        <h2>Forensic Autopsy</h2>
+        <p>Cause of death: lethal CO₂ intoxication.</p>
+    `,
+
+    transcripts: `
+        <h2>Interrogations</h2>
+        <p>"I was in the nap room..."</p>
+    `,
+
+    logs: `
+        <h2>Server Room Access Logs</h2>
+        <pre>
+02:30:11 ARJUN_99 Entry
+02:45:00 MAYA_01 Entry
+02:45:05 MAYA_01 Exit
+        </pre>
+    `
+};
